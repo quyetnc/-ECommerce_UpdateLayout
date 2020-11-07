fbda25f738d9368b67dac1ac68abe290f8df808e
Quyet Nguyen Canh (FIS TDC HCM) <quyetnc2@fpt.com.vn>
2020-11-07 14:40:49 +0700

    first commit

---
+++ ThuongMaiDienTu/Models/EditCardModal.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace ThuongMaiDienTu.Models
+{
+    public class EditCardModal
+    {
+        public int idProduct { get; set; }
+        public string type { get; set; }
+    }
+}
